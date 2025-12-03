0d3f7b42a1b3e48e5f51bf4271ce3048d325d47d
Kiju <dev@jakubkijek.com>
2025-12-03 22:28:18 +0100

    Stop work on sser

--- apps/example/main.cpp
+++ apps/example/main.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-
-#include "sfmt.hpp"
-
-int main() {
-    sfmt::Object sexpr = sfmt::Object::table(
-        {{"key1", sfmt::Object::value("value1")},
-         {"key2",
-          sfmt::Object::list({sfmt::Object::value("value2"), sfmt::Object::value("value3")})}});
-
-    std::cout << sexpr.fmt() << std::endl;
-    std::cout << "\n";
-    std::cout << sexpr.fmt_pretty() << std::endl;
-
-    return 0;
-}
+int main() { return 0; }
